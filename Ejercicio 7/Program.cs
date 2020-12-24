82b4a7cb537e27a60957afdcc15240075fcbd750
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 13:21:21 +0100

    Ejercicio 7, Unidad 4

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("El numero es: {0}", i);
+            }
+        }
+    }
+}
