85a7428d49d1e83a9a6635de2fff260916657c8a
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 14:06:28 +0100

    Ejercicio 11, Unidad 4

---
+++ Ejercicio 11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1;
+            int Num2;
+
+            Console.WriteLine("Introduzca el primer numero: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduzca el segundo numero: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Num1>Num2)
+            {
+                Console.WriteLine("{0} es mayor que {1}",Num1,Num2);
+            }else if (Num2 > Num1)
+            {
+                Console.WriteLine("{0} es mayor que {1}", Num2, Num1);
+            }
+            else if(Num1==Num2 || Num2 == Num1)
+            {
+                Console.WriteLine("Los dos numeros son iguales");
+            }
+         
+            
+
+
+        }
+    }
+}
