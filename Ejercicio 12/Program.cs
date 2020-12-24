aa483d1d969efb2d24c53b76c92e333df3a17132
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 14:15:19 +0100

    Ejercicio 12, Unidad 4

---
+++ Ejercicio 12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre");
+            String Nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido {0}",Nombre);
+        }
+    }
+}
