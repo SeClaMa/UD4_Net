3ee124535745837b41d107bb74bf1a0a1fdfd4de
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 13:45:10 +0100

    Ejercicio 10, Unidad 4

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine("Este numero es par o divisible entre 3, {0}", i);
+                }
+
+            }
+        }
+    }
+}
