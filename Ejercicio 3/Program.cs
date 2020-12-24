276b99c0206c59c46273dfbfd7091ceef94df3c8
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 10:32:58 +0100

    Ejercicio 3, Unidad 4

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escriba su nombre: ");
+            String Nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su edad: ");
+            int Edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Te llamas {0} y tienes {1} años de edad.", Nombre, Edad);
+        }
+    }
+    }
+
