4e98975400fdda7dad4c3e363e3d38053fc211c9
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-24 10:18:12 +0100

    Ejercicio 2, Unidad 4

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escriba su nombre: ");
+            String Nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su ciudad: ");
+            String Ciudad = Console.ReadLine();
+
+            Console.WriteLine("Hola {0}, bienvenido a {1}.",Nombre, Ciudad);
+        }
+    }
+}
