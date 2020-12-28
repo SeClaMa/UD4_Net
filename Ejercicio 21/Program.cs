de0c28c21016270d8770b96fd98031a8247e974e
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-28 10:50:24 +0100

    Ejercico 21, Unidad 4

---
+++ Ejercicio 21/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ejercicio_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el dia de la semana: ");
+            String  Dia = Console.ReadLine();
+
+            
+            switch (Dia.ToLower())
+            {
+                case "lunes":
+                    Console.WriteLine("Es dia Laboral");
+                    break;
+                case "martes":
+                    Console.WriteLine("Es dia Laboral");
+                    break;
+                case "miercoles":
+                    Console.WriteLine("Es dia Laboral");
+                    break;
+                case "jueves":
+                    Console.WriteLine("Es dia Laboral");
+                    break;
+                case "viernes":
+                    Console.WriteLine("Es dia Laboral");
+                    break;
+                case "sabado":
+                    Console.WriteLine("No es dia Laboral");
+                    break;
+                case "domingo":
+                    Console.WriteLine("No es dia Laboral");
+                    break;
+
+                default:
+                    Console.WriteLine("No ha indicado el dia correctamente, nulo");
+                    break;
+            }
+        }
+    }
+}
