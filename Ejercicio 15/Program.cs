e08d7bbd0bbe1b5a969ffe5ebfc4580001c0507a
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-28 09:41:28 +0100

    Ejercicio 15, Unidad 4

---
+++ Ejercicio 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("Por favor, indique el numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Es divisible entre 2");
+            }else if (num1 % 2 !=0 )
+            {
+                Console.WriteLine("No es divisible entre 2");
+            }
+
+        }
+    }
+}
