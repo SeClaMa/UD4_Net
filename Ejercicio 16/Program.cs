91c667868496b2e96255a2839e559ae8aa5709a7
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-28 09:54:06 +0100

    Ejercicio 16, unidad 4

---
+++ Ejercicio 16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            double IVA = 21;
+            Console.WriteLine("Por favor, introduzca el precio del producto: ");
+
+            precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("El producto final con IVA es {0}", precio*IVA/100+precio);
+
+        }
+    }
+}
