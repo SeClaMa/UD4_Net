09e302dc26f0fe7cdaae211493ffcc8a78ac511c
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-28 09:28:07 +0100

    Ejercicio 14, Unidad 4

---
+++ Ejercicio 14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, area;
+            Console.Write("Escriba el radio del circulo : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            //area = 3.14 * r * r;    
+            area = Math.PI * Math.Pow(r, 2);      
+            Console.WriteLine("El area del circulo es : {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
