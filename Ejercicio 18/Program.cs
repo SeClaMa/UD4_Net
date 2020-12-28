d650d58e7111eaca6e62eac615fba58e9cc77a94
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-28 10:00:20 +0100

    Ejercicio 18, unidad 4

---
+++ Ejercicio 18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("El numero es: {0}",i);
+            }
+            
+        }
+    }
+}
